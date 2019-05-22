93a0bdab9b30fd6b45656d854597fad2628bf0a3
iphipan <50053458+iphipan@users.noreply.github.com>
2019-05-22 20:36:05 +0200

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?
+
+function nukata_enqueue_styles() {
+  wp_register_style('bootstrap', get_template_directory_uri(). '/bootstrap/css/bootstrap.min.css');
+  $dependencies = array('bootstrap');
+  wp_enqueue_style('nukata-style', get_stylesheet_uri(), $dependencies);
+}
+
+function nukata_enqueue_scripts() {
+    $dependencies = array('jquery');
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '4.3.1', true);
+}
+
+add_action('wp_enqueue_scripts', 'nukata_enqueue_styles');
+add_action('wp_enqueue_scripts', 'nukata_enqueue_scripts');
+
+?>
