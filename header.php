3eb34c34ec6edc87121e73c5496694e0c8c798f7
iphipan <50053458+iphipan@users.noreply.github.com>
2019-05-24 22:06:10 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -1,30 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-  <meta charset="utf-8">
+  <meta charset="<?php bloginfo( 'charset' );?>">
   <meta http-equiv="X-UA-Compatible" content="IE-edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- the above 3 meta tags must come first in the head; any other head content must come after these tags -->
   <meta name="description" content="">
-  <meta name="author" content="">
-  <link rel="icon" href="../../favicon.ico">
-
-  <title>Blog Template for Bootstrap</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- IE10 viewport hack for Surface/desktop Windows8 bug -->
-  <link href="../..assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="blog.css" rel="stylesheet">
 
   <!--HTML5 shim and Respond.js for IE8 support of HTML5 element and media queries -->
   <!--[if lt IE 9]-->
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/htmlshiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <!--[endif]-->
+  <?php wp_head(); ?>
 </head>
 
 <body>
