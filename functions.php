1b3b8920d7f0495fbbe00e914ba48ea291bde551
iphipan <50053458+iphipan@users.noreply.github.com>
2019-05-27 20:30:02 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,23 +1,22 @@
-<?
+<?php
 
 function nukata_enqueue_styles() {
-  wp_register_style('bootstrap', get_template_directory_uri(). '/bootstrap/css/bootstrap.min.css');
-  $dependencies = array('bootstrap');
-  wp_enqueue_style('nukata-style', get_stylesheet_uri(), $dependencies);
+    wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
+    $dependencies = array('bootstrap');
+	wp_enqueue_style( 'nukata-style', get_stylesheet_uri(), $dependencies );
 }
 
 function nukata_enqueue_scripts() {
     $dependencies = array('jquery');
-    wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '4.3.1', true);
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '', true );
 }
 
-add_action('wp_enqueue_scripts', 'nukata_enqueue_styles');
-add_action('wp_enqueue_scripts', 'nukata_enqueue_scripts');
+add_action( 'wp_enqueue_scripts', 'nukata_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'nukata_enqueue_scripts' );
 
-function nukata_wp_setup( {
-  add_theme_support( 'title-tag') ;
+function nukata_wp_setup() {
+    add_theme_support( 'title-tag' );
 }
 
-add_action('after_setup_theme', 'nukata_wp_setup') ;
-
+add_action( 'after_setup_theme', 'nukata_wp_setup' );
 ?>
--- index.php
+++ index.php
@@ -1,30 +1,26 @@
 <?php get_header(); ?>
 <div class="col-sm-8 blog-main">
 
-<?php
-if ( have_posts()) {
-while (have_posts() ): the_post();
-?>
-<div class="blog-post">
-<h2 class="blog-post-title"><?php the_title(); ?></h2>
-<p class="blog-post-meta"><?php the_date(); ?> by <?php the_author(); ?></p>
-<?php the_content();?>
-</div><!--/.blog-post -->
-<?php
-endwhile
-}
-?>
-<nav>
-  <ul class="pager>
-    <li><?php next_posts_link('Previous'); ?> </li>
-    <li><?php next_posts_link('Next'); ?> </li>
-  </ul>
-</nav>
+    <?php
+    if ( have_posts() ) {
+        while ( have_posts() ) : the_post();
+    ?>
+    <div class="blog-post">
+        <h2 class="blog-post-title"><?php the_title(); ?></h2>
+        <p class="blog-post-meta"><?php the_date(); ?> by <?php the_author(); ?></p>
+        <?php the_content(); ?>
+    </div><!-- /.blog-post -->
+    <?php
+        endwhile;
+    }
+    ?>
 
-</div><-- /.blog-main -->
-<? php get_footer(); ?>
+    <nav>
+        <ul class="pager">
+            <li><?php next_posts_link('Previous'); ?></li>
+            <li><?php previous_posts_link('Next'); ?></li>
+        </ul>
+    </nav>
 
-
-
-
-<!-- /.blog-main -->
+</div><!-- /.blog-main -->
+<?php get_footer(); ?>
